2a5ca5d012da0554c5b8096677b681dabccc7b99
Lunev Artem <artem.e.lunev2000@gmail.com>
2021-06-05 12:19:29 +0300

    feat: task improvement

---
+++ message.py
@@ -0,0 +1,6 @@
+class Message:
+    def __init__(self, message, name, time, author):
+        self.message = message
+        self.name = name
+        self.time = time
+        self.author = author
