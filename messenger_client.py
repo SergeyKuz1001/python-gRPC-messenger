3d68edef8c950fe4dfda736050503dde9332b17f
Lunev Artem <artem.e.lunev2000@gmail.com>
2021-06-16 17:52:00 +0300

    feat: added first version

--- messenger_client.py
+++ messenger_client.py
@@ -14,10 +14,13 @@ from message import Message
 
 class Client:
     def __init__(self, name, host, port):
-        self.name = name
-        self.server_name = None
+        self.my_name = name
+        self.companion_name = None
         self.messages = []
         self.client = None
+        self.mes = []
+        self.my_stream = iter(self.mes)
+        self.companion_stream = None
 
         self.run(host, port)
 
@@ -25,32 +28,42 @@ class Client:
         channel = grpc.insecure_channel(f"{host}:{port}")
         self.client = messenger_pb2_grpc.MessengerStub(channel)
 
-        request = messenger_pb2.MessengerNameRequest(name=self.name)
+        request = messenger_pb2.MessengerNameRequest(name=self.my_name)
         resp = self.client.startMessaging(request)
         if resp.connected:
-            self.server_name = resp.name
+            self.companion_name = resp.name
         else:
             print("server already has connected client")
             return
 
         self.main_window = ChatWindow(self, 'Client')
+        self.companion_stream = self.client.sendMessage(self.my_stream)
+
         th = threading.Thread(target=self.get_messages, daemon=True)
         th.start()
-        
+
+        th1 = threading.Thread(target=self.send_messages, daemon=True)
+        th1.start()
+
         while True:
             message = self.main_window.processing()
             request = messenger_pb2.MessengerMessage(message=message)
-            self.client.getMessage(request, timeout=1)
-            self.main_window.print(Message(message, self.name, datetime.datetime.now(), 'client'))
-            self.messages.append(Message(message, self.name, datetime.datetime.now(), 'client'))
-                 
+            self.mes.append(request)
+            self.messages.append(Message(message, self.my_name, datetime.datetime.now(), 'client'))
+
     def get_messages(self):
-        resp = self.client.sendMessage(messenger_pb2.Empty())
         try:
-            for mes in resp:
-                self.main_window.print(Message(mes.message, self.server_name, datetime.datetime.now(), 'server'))
-                self.messages.append(Message(mes.message, self.server_name, datetime.datetime.now(), 'server'))
+            for mes in self.companion_stream:
+                self.main_window.print(Message(mes.message, self.companion_name, datetime.datetime.now(), 'server'))
+                self.messages.append(Message(mes.message, self.companion_name, datetime.datetime.now(), 'server'))
         except grpc.RpcError as rpc_error_call:
             self.main_window.window.close()
             sg.popup_ok("Server dropped connection", keep_on_top = True)
             os.exit(0)
+
+    def send_messages(self):
+        while True:
+            message = self.main_window.processing()
+            request = messenger_pb2.MessengerMessage(message=message)
+            self.mes.append(request)
+            self.messages.append(Message(message, self.my_name, datetime.datetime.now(), 'client'))
--- messenger_server.py
+++ messenger_server.py
@@ -15,23 +15,26 @@ from message import Message
 
 class Server(messenger_pb2_grpc.MessengerServicer):
     def __init__(self, name):
-        self.name = name
+        self.my_name = name
+        self.companion_name = None
         self.connected = False
-        self.client_name = None
         self.messages = []
         self.main_window = None
         self.stop_event = None
+        self.mes = []
+        self.my_stream = iter(self.mes)
+        self.companion_stream = None
 
         self.serve()
 
     def startMessaging(self, request, context):
         logging.info(f'got name "{request.name}"')
         if not self.connected:
-            self.client_name = request.name
+            self.companion_name = request.name
             self.connected = True
 
             self.main_window = ChatWindow(self, 'Server')
-            return messenger_pb2.MessengerNameResponse(name=self.name, connected=True)
+            return messenger_pb2.MessengerNameResponse(name=self.my_name, connected=True)
 
         else:
             return messenger_pb2.MessengerNameResponse(connected=False)
@@ -43,17 +46,12 @@ class Server(messenger_pb2_grpc.MessengerServicer):
         return messenger_pb2.Empty()
         
     def sendMessage(self, request_iterator, context):
-        while self.connected:
-            mes = self.main_window.processing()
-            self.main_window.print(Message(mes, self.name, datetime.datetime.now(), 'server'))
-            self.messages.append(Message(mes, self.name, datetime.datetime.now(), 'server'))
-            yield messenger_pb2.MessengerMessage(message=mes)
+        self.companion_stream = request_iterator
+        th = threading.Thread(target=self.get_messages, daemon=True)
+        th.start()
 
-    def getMessage(self, request, context):
-        logging.info(f'got message "{request.message}" from {self.client_name}')
-        self.main_window.print(Message(request.message, self.client_name, datetime.datetime.now(), 'client'))
-        self.messages.append(Message(request.message, self.client_name, datetime.datetime.now(), 'client'))
-        return messenger_pb2.Empty()
+        th1 = threading.Thread(target=self.send_messages, daemon=True)
+        th1.start()
 
     def serve(self):
         self.stop_event = threading.Event()
@@ -68,3 +66,20 @@ class Server(messenger_pb2_grpc.MessengerServicer):
         if not self.connected:
             sg.popup_ok("Client disconnected", keep_on_top = True)
         os._exit(0)
+
+    def get_messages(self):
+        try:
+            for mes in self.companion_stream:
+                self.main_window.print(Message(mes.message, self.companion_name, datetime.datetime.now(), 'server'))
+                self.messages.append(Message(mes.message, self.companion_name, datetime.datetime.now(), 'server'))
+        except grpc.RpcError as rpc_error_call:
+            self.main_window.window.close()
+            sg.popup_ok("connection dropped", keep_on_top=True)
+            os.exit(0)
+
+    def send_messages(self):
+        while True:
+            message = self.main_window.processing()
+            request = messenger_pb2.MessengerMessage(message=message)
+            self.mes.append(request)
+            self.messages.append(Message(message, self.my_name, datetime.datetime.now(), 'client'))
